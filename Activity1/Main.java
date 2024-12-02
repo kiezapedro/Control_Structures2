92acfcb156b0b61842e79cb8b11bd673bd6a1e8c
kiezapedro <lsfykkiezapedro@gmail.com>
2024-12-02 17:22:54 -0600

    Activities done in class

---
+++ Activity1/Main.java
@@ -0,0 +1,18 @@
+package Activity1;
+
+class Main {
+    public static void main(String[] args) {
+        boolean sunny = false;
+        boolean hot = true;
+
+        if (sunny) {
+            if (hot) {
+                System.out.println("Use cap & half sleeves");
+            } else{
+                System.out.println("Use half sleeves");
+            }
+        } else{
+            System.out.println("Take Rain-Coat for safety");
+        }
+    }
+}
